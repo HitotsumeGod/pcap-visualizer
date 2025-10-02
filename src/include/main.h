9437516f288bd2696f78c18433119c0443369d32
peternaylor <peter@oldtiogafarm.com>
2025-10-01 21:49:06 -0400

    major work on frame format

--- src/include/main.h
+++ src/include/main.h
@@ -3,37 +3,79 @@
 
 #include "ptools.h"
 #include <stdio.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
 
-struct pcap_hdr {
-        dword magic;
-        word majorv;
-        word minorv;
-        dword res1;
-        dword res2;
-        dword snaplen;
-        byte fcs:3;
-        byte f:1;
-        word zero:12;
-        word linktype;
+struct network_dgram {
+        union {
+                struct iphdr            *iph;
+                struct ip6hdr           *ip6h;
+        } *hdr;
+        struct {
+                union {
+                        struct icpmhdr          *icmph;
+                        struct icmp6hdr         *icmp6h;
+                        struct tcphdr           *tcph;
+                        struct udphdr           *udph;
+                } *hdr;
+                byte        *body;
+        } transport_packet;
 };
 
-struct pcap_rec {
-        dword timestamp_big;
-        dword timestamp_small;
-        dword capd_plen;
-        dword og_plen;
-        byte *daten;
-        struct pcap_rec *next;
+union linklayer_frame {
+        struct {
+                struct {
+                        byte            proto_vers:2;
+                        byte            type:2;
+                        byte            subtype:4;
+                        byte            to_ds:1;
+                        byte            from_ds:1;
+                        byte            more_frag:1;
+                        byte            retry:1;
+                        byte            power_mng:1;
+                        byte            more_data:1;
+                        byte            prot_frame:1;
+                        byte            order:1;
+                } *frame_control;
+                word                    duration;
+                byte                    addr1[6];
+                byte                    addr2[6];
+                byte                    addr3[6];
+                word                    seq_ctrl;
+                byte                    addr4[6];
+                struct network_dgram    *body;
+                dword                   fcs;
+        } _80211_frame;
+        struct {
+                struct ethhdr           *hdr;
+                struct network_dgram    *body;
+                dword                   crc;
+        } etherii_frame;
 };
 
 struct pcap {
-        struct pcap_hdr *header;
-        struct pcap_rec *records;
+        struct pcap_hdr {
+                dword                 magic;
+                word                  majorv;
+                word                  minorv;
+                dword                 thiszone;
+                dword                 sigfigs;
+                dword                 snaplen;
+                dword                 network;
+        } *hdr;
+        struct pcap_rec {
+                dword                 tstamp_big;
+                dword                 tstamp_small;
+                dword                 capd_plen;
+                dword                 og_plen;
+                byte                  *daten;
+                struct pcap_rec       *next;
+        } *recs;
 };
 
 #define         REC_CONST_SIZ sizeof(dword) * 4
 
-extern struct errep *parse_pcap_file(byte *pcap, size_t length, struct pcap_hdr **header, struct pcap_rec **records);
+extern struct errep *parse_pcap_file(byte *pcap, size_t length, struct pcap **res);
 
 extern struct errep *print_pcap_info(struct pcap *cap);
 
--- src/main/print.c
+++ src/main/print.c
@@ -7,11 +7,11 @@ struct errep *print_pcap_info(struct pcap *cap)
         struct errep *err;
         char *fnname = "print_pcap_info()";
 
-        printf("PCAP Magic Number is : %d\n", cap -> header -> magic);
-        printf("PCAP Major Version is : %d\n", cap -> header -> majorv);
-        printf("PCAP Minor Version is : %d\n", cap -> header -> minorv);
+        printf("PCAP Magic Number is : 0x%02X\n", cap -> header -> magic);
+        printf("PCAP Major Version is : 0x%02X\n", cap -> header -> majorv);
+        printf("PCAP Minor Version is : 0x%02X\n", cap -> header -> minorv);
         printf("PCAP Snapshot Length is : %d\n", cap -> header -> snaplen);
-        printf("PCAP LinkType is : %d\n", cap -> header -> linktype);
+        printf("PCAP LinkType is : 0x%02X\n", cap -> header -> network);
         ERREP(err, fnname, NULL);
         return err;
 }
