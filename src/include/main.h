dd7828a10cbefed70cf7f1e0fd398a3bc822afcc
peternaylor <peter@oldtiogafarm.com>
2025-10-02 06:49:23 -0400

    splitting functionality between ptools

--- src/include/main.h
+++ src/include/main.h
@@ -3,55 +3,6 @@
 
 #include "ptools.h"
 #include <stdio.h>
-#include <netinet/ether.h>
-#include <netinet/ip.h>
-
-struct network_dgram {
-        union {
-                struct iphdr            *iph;
-                struct ip6hdr           *ip6h;
-        } *hdr;
-        struct {
-                union {
-                        struct icpmhdr          *icmph;
-                        struct icmp6hdr         *icmp6h;
-                        struct tcphdr           *tcph;
-                        struct udphdr           *udph;
-                } *hdr;
-                byte        *body;
-        } transport_packet;
-};
-
-union linklayer_frame {
-        struct {
-                struct {
-                        byte            proto_vers:2;
-                        byte            type:2;
-                        byte            subtype:4;
-                        byte            to_ds:1;
-                        byte            from_ds:1;
-                        byte            more_frag:1;
-                        byte            retry:1;
-                        byte            power_mng:1;
-                        byte            more_data:1;
-                        byte            prot_frame:1;
-                        byte            order:1;
-                } *frame_control;
-                word                    duration;
-                byte                    addr1[6];
-                byte                    addr2[6];
-                byte                    addr3[6];
-                word                    seq_ctrl;
-                byte                    addr4[6];
-                struct network_dgram    *body;
-                dword                   fcs;
-        } _80211_frame;
-        struct {
-                struct ethhdr           *hdr;
-                struct network_dgram    *body;
-                dword                   crc;
-        } etherii_frame;
-};
 
 struct pcap {
         struct pcap_hdr {
